686b7f6803cbc89421846bf9554f01da5e544050
dernestbank <dernestbank@yahoo.com>
2025-01-31 14:24:42 -0500

    the env import was the problem

--- ATJ/config.py
+++ ATJ/config.py
@@ -16,15 +16,30 @@ mt5_credentials = {
     'exe_path': None
 }
 
+login2 = 192918303
+password2 = 'Demo@1234'
+server2 = 'Exness-MT5Trial'
 
 def initialize_mt5():
     import MetaTrader5 as mt5
+    mt5.initialize()
 
-    mt5.initialize(mt5_credentials['exe_path'])
-    mt5.login(mt5_credentials['login'], mt5_credentials['password'], mt5_credentials['server'])
-    print(mt5.account_info())
+    # mt5.initialize(mt5_credentials['exe_path'])
+    # mt5.login(mt5_credentials['login'], mt5_credentials['password'], mt5_credentials['server'])
+    # print(mt5.account_info())
+    # print(mt5_credentials)
+    
+    if not mt5.login(login2, password2, server2):
+        print("Failed to connect to the server")
+    else:
+        print("Connected to the server successfully")
+        print(mt5.account_info())
     
     return mt5
 
+# initialize_mt5()
 
-initialize_mt5()
+if __name__ == '__main__':
+    initialize_mt5()
+    
+    
